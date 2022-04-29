c79e5d511aff16cf9e54cea89a5049b85d7869e8
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-04-29 22:10:35 +0200

    Test `uint` compat

--- test.mjs
+++ test.mjs
@@ -3,11 +3,29 @@ import c from 'compact-encoding'
 
 import bitfield from './index.js'
 
-test('number', async (t) => {
-  const enc = bitfield(4)
+test('uint compat', async (t) => {
+  t.is(
+    c.decode(c.uint,
+      // bitfield: fd fc 00
+      // uint: fc
+      c.encode(bitfield(8), 0b1111_1100)
+    ),
+    0b1111_1100
+  )
 
-  t.alike(c.encode(enc, 0b1), Buffer.from([0b1]))
-  t.alike(c.encode(enc, 0b11), Buffer.from([0b11]))
-  t.alike(c.encode(enc, 0b111), Buffer.from([0b111]))
-  t.alike(c.encode(enc, 0b1111), Buffer.from([0b1111]))
+  t.is(
+    c.decode(c.uint,
+      // bitfield, uint: fd fe 00
+      c.encode(bitfield(8), 0b1111_1110)
+    ),
+    0b1111_1110
+  )
+
+  t.is(
+    c.decode(c.uint,
+      // bitfield, uint: fd fe 00
+      c.encode(bitfield(16), 0b1111_1111_0011_0011)
+    ),
+    0b1111_1111_0011_0011
+  )
 })
