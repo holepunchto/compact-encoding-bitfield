2df33c26548edf1de49ad52adbe72d5dfad16e7f
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-09-12 09:20:45 +0200

    Update `brittle`

---
+++ fixtures/test.snapshot.cjs
@@ -0,0 +1,111 @@
+/* eslint-disable */
+
+const { createTypedArray } = require('brittle')
+
+exports['bitfield(1) - ABI - 0'] = createTypedArray(Uint8Array, 'AQ==')
+
+exports['bitfield(2) - ABI - 0'] = createTypedArray(Uint8Array, 'Aw==')
+
+exports['bitfield(3) - ABI - 0'] = createTypedArray(Uint8Array, 'BQ==')
+
+exports['bitfield(4) - ABI - 0'] = createTypedArray(Uint8Array, 'CQ==')
+
+exports['bitfield(5) - ABI - 0'] = createTypedArray(Uint8Array, 'EQ==')
+
+exports['bitfield(6) - ABI - 0'] = createTypedArray(Uint8Array, 'IQ==')
+
+exports['bitfield(7) - ABI - 0'] = createTypedArray(Uint8Array, 'QQ==')
+
+exports['bitfield(8) - ABI - 0'] = createTypedArray(Uint8Array, '/YEA')
+
+exports['bitfield(9) - ABI - 0'] = createTypedArray(Uint8Array, '/QEB')
+
+exports['bitfield(10) - ABI - 0'] = createTypedArray(Uint8Array, '/QEC')
+
+exports['bitfield(11) - ABI - 0'] = createTypedArray(Uint8Array, '/QEE')
+
+exports['bitfield(12) - ABI - 0'] = createTypedArray(Uint8Array, '/QEI')
+
+exports['bitfield(13) - ABI - 0'] = createTypedArray(Uint8Array, '/QEQ')
+
+exports['bitfield(14) - ABI - 0'] = createTypedArray(Uint8Array, '/QEg')
+
+exports['bitfield(15) - ABI - 0'] = createTypedArray(Uint8Array, '/QFA')
+
+exports['bitfield(16) - ABI - 0'] = createTypedArray(Uint8Array, '/QGA')
+
+exports['bitfield(17) - ABI - 0'] = createTypedArray(Uint8Array, '/gEAAQA=')
+
+exports['bitfield(18) - ABI - 0'] = createTypedArray(Uint8Array, '/gEAAgA=')
+
+exports['bitfield(19) - ABI - 0'] = createTypedArray(Uint8Array, '/gEABAA=')
+
+exports['bitfield(20) - ABI - 0'] = createTypedArray(Uint8Array, '/gEACAA=')
+
+exports['bitfield(21) - ABI - 0'] = createTypedArray(Uint8Array, '/gEAEAA=')
+
+exports['bitfield(22) - ABI - 0'] = createTypedArray(Uint8Array, '/gEAIAA=')
+
+exports['bitfield(23) - ABI - 0'] = createTypedArray(Uint8Array, '/gEAQAA=')
+
+exports['bitfield(24) - ABI - 0'] = createTypedArray(Uint8Array, '/gEAgAA=')
+
+exports['bitfield(25) - ABI - 0'] = createTypedArray(Uint8Array, '/gEAAAE=')
+
+exports['bitfield(26) - ABI - 0'] = createTypedArray(Uint8Array, '/gEAAAI=')
+
+exports['bitfield(27) - ABI - 0'] = createTypedArray(Uint8Array, '/gEAAAQ=')
+
+exports['bitfield(28) - ABI - 0'] = createTypedArray(Uint8Array, '/gEAAAg=')
+
+exports['bitfield(29) - ABI - 0'] = createTypedArray(Uint8Array, '/gEAABA=')
+
+exports['bitfield(30) - ABI - 0'] = createTypedArray(Uint8Array, '/gEAACA=')
+
+exports['bitfield(31) - ABI - 0'] = createTypedArray(Uint8Array, '/gEAAEA=')
+
+exports['bitfield(32) - ABI - 0'] = createTypedArray(Uint8Array, '/gEAAIA=')
+
+exports['bitfield(33) - ABI - 0'] = createTypedArray(Uint8Array, '/wEAAAABAAAA')
+
+exports['bitfield(34) - ABI - 0'] = createTypedArray(Uint8Array, '/wEAAAACAAAA')
+
+exports['bitfield(35) - ABI - 0'] = createTypedArray(Uint8Array, '/wEAAAAEAAAA')
+
+exports['bitfield(36) - ABI - 0'] = createTypedArray(Uint8Array, '/wEAAAAIAAAA')
+
+exports['bitfield(37) - ABI - 0'] = createTypedArray(Uint8Array, '/wEAAAAQAAAA')
+
+exports['bitfield(38) - ABI - 0'] = createTypedArray(Uint8Array, '/wEAAAAgAAAA')
+
+exports['bitfield(39) - ABI - 0'] = createTypedArray(Uint8Array, '/wEAAABAAAAA')
+
+exports['bitfield(40) - ABI - 0'] = createTypedArray(Uint8Array, '/wEAAACAAAAA')
+
+exports['bitfield(41) - ABI - 0'] = createTypedArray(Uint8Array, '/wEAAAAAAQAA')
+
+exports['bitfield(42) - ABI - 0'] = createTypedArray(Uint8Array, '/wEAAAAAAgAA')
+
+exports['bitfield(43) - ABI - 0'] = createTypedArray(Uint8Array, '/wEAAAAABAAA')
+
+exports['bitfield(44) - ABI - 0'] = createTypedArray(Uint8Array, '/wEAAAAACAAA')
+
+exports['bitfield(45) - ABI - 0'] = createTypedArray(Uint8Array, '/wEAAAAAEAAA')
+
+exports['bitfield(46) - ABI - 0'] = createTypedArray(Uint8Array, '/wEAAAAAIAAA')
+
+exports['bitfield(47) - ABI - 0'] = createTypedArray(Uint8Array, '/wEAAAAAQAAA')
+
+exports['bitfield(48) - ABI - 0'] = createTypedArray(Uint8Array, '/wEAAAAAgAAA')
+
+exports['bitfield(49) - ABI - 0'] = createTypedArray(Uint8Array, '/wEAAAAAAAEA')
+
+exports['bitfield(50) - ABI - 0'] = createTypedArray(Uint8Array, '/wEAAAAAAAIA')
+
+exports['bitfield(51) - ABI - 0'] = createTypedArray(Uint8Array, '/wEAAAAAAAQA')
+
+exports['bitfield(52) - ABI - 0'] = createTypedArray(Uint8Array, '/wEAAAAAAAgA')
+
+exports['bitfield(53) - ABI - 0'] = createTypedArray(Uint8Array, '/wEAAAAAABAA')
+
+/* eslint-enable */
