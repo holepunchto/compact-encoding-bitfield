e45e96401b783e33ce09ac60821e758891169770
Mikkel Malmberg <mikkel@brnbw.com>
2025-10-06 08:57:52 +0200

    Migrate to Prettier (#2)

--- test.mjs
+++ test.mjs
@@ -9,37 +9,21 @@ for (let n = 1; n <= 53; n++) {
     const i = 2 ** (n - 1) + (n === 1 ? 0 : 1) // Set first and last bit
     const b = toBuffer(i, n)
 
-    t.alike(
-      c.decode(bitfield(n), c.encode(bitfield(n), i)),
-      b,
-      i.toString(2)
-    )
+    t.alike(c.decode(bitfield(n), c.encode(bitfield(n), i)), b, i.toString(2))
 
-    t.alike(
-      c.decode(bitfield(n), c.encode(bitfield(n), b)),
-      b,
-      util.inspect(b)
-    )
+    t.alike(c.decode(bitfield(n), c.encode(bitfield(n), b)), b, util.inspect(b))
 
     t.snapshot(c.encode(bitfield(n), b), 'ABI')
 
     t.test('uint compatibility', async (t) => {
-      t.is(
-        c.decode(c.uint, c.encode(bitfield(n), i)),
-        i,
-        'encode'
-      )
+      t.is(c.decode(c.uint, c.encode(bitfield(n), i)), i, 'encode')
 
-      t.alike(
-        c.decode(bitfield(n), c.encode(c.uint, i)),
-        b,
-        'decode'
-      )
+      t.alike(c.decode(bitfield(n), c.encode(c.uint, i)), b, 'decode')
     })
   })
 }
 
-function toBuffer (b, length) {
+function toBuffer(b, length) {
   if (length <= 8) return c.encode(c.uint8, b)
   if (length <= 16) return c.encode(c.uint16, b)
   if (length <= 32) return c.encode(c.uint32, b)
